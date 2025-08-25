574456c59e16b2560e8eeb6e099203605813ccc8
AADARSH GAIKWAD <30507087+AADARSH96@users.noreply.github.com>
2025-08-25 16:09:13 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aadarsh96.github.io',
+  site: 'https://astronaut.github.io',
+  base: 'aadarsh96.github.io',
 });
